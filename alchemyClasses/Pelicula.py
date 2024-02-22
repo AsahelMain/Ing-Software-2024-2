368f5b7bae2c778e293c73bdaa0a6b9fcf15eb2a
Asahel <asahelmain@ciencias.unam.mx>
2024-02-22 12:24:47 -0600

    Agrega esquemas ORM

---
+++ alchemyClasses/Pelicula.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from alchemyClasses import db
+
+class Pelicula(db.Model):
+    __tablename__ = 'peliculas'
+    idPelicula = Column(Integer, primary_key=True)
+    nombre = Column(String(200))
+    genero = Column(String(45), nullable=True)
+    duracion = Column(Integer, nullable=True)
+    inventario = Column(Integer, default=1)
+
+    def __init__(self, nombre, genero=None,duracion=None,inventario=1):
+        self.nombre = nombre
+        self.genero = genero
+        self.duracion = duracion
+        self.inventario = inventario
+    
+    def __str__(self):
+        return f'Nombre: {self.nombre}\nGenero: {self.genero}\nDuracion: {self.duracion}'
+    
