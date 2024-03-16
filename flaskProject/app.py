67dd54bbf2cf9971af6911e5a59a443ae94ebeba
Asahel <asahelmain@ciencias.unam.mx>
2024-03-15 23:19:24 -0600

    Arregla error ortografico en app.py

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
 from alchemyClasses import db
-from contollers.PrimerControlador import mi_primer_blueprint
-from contollers.ControllerAlumno import alumno_blueprint
+from controllers.PrimerControlador import mi_primer_blueprint
+from controllers.ControllerAlumno import alumno_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://asmc:Develooper123!@localhost:3306/ing_soft'
