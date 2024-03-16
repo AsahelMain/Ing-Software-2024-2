49990f238c16a325f73b234638b8ca3ab6451095
Asahel <asahelmain@ciencias.unam.mx>
2024-03-16 15:40:42 -0600

    Registra userblueprint en app.py

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from alchemyClasses import db
 from controllers.PrimerControlador import mi_primer_blueprint
 from controllers.ControllerAlumno import alumno_blueprint
+from controllers.ControladorUsuario import blueprint_usuario
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://asmc:Develooper123!@localhost:3306/ing_soft'
@@ -12,6 +13,7 @@ app.config.from_mapping(
 db.init_app(app)
 app.register_blueprint(mi_primer_blueprint)
 app.register_blueprint(alumno_blueprint)
+app.register_blueprint(blueprint_usuario)
 
 @app.route('/')
 def hello_world():  # put application's code here
